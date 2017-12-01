4e7bae98c2e1d9cb4e349d9ebc5c1d7f8954a3d3
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-12-01 13:49:31 +0100

    Fix extractor

--- bin/test.js
+++ bin/test.js
@@ -1,10 +1,8 @@
 const { extractFromGoogle, extractKeywords } = require('../lib/extractor');
 const debug = require('debug')('MemQL:test');
 
-extractFromGoogle('java data binding')
-    .then(res => {
-        extractKeywords(res[0]);
-    })
+extractFromGoogle('java android null pointer exception')
+    .then(res => extractKeywords(res[0]))
     .then(res => {
         debug('Extracted keywords :', res);
     }).catch(debug);
