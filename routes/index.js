b99b9f78deff277fe777ff4cd3c3cb3832f727d5
Nathan Casals <nathan.casals@smartprospective.com>
2017-12-05 09:03:55 +0100

    Added html index + display

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const debug = require('debug')('MemeMQL:express');
+const data = require('../data/d3_test_data');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    debug("dirname : " + __dirname);
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+});
+
+
+/* GET users listing. */
+router.get('/test', function (req, res, next) {
+    res.json(data);
 });
 
 module.exports = router;
