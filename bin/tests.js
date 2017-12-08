abd8fc44c786ef5b538ee4ea99f44c7ba1f9f36f
Nathan Casals <nathan.casals@smartprospective.com>
2017-12-08 07:44:44 +0100

    Added graph display with d3

---
+++ bin/tests.js
@@ -0,0 +1,10 @@
+const {constructGraph} = require('../lib/graph.js');
+const {constructGraphs} = require('../lib/graph.js');
+const {constructJsonData} = require('../lib/graph.js');
+const test = {
+    "url1": [["s1", "p1", "o1"], ["s2", "p2", "s1"], ["s1", "p3", "s2"]],
+    "url2": [["s3", "p3", "o2"], ["s2", "p2", "o1"], ["s4", "p3", "o2"]]
+};
+
+const constructedGraphs = constructGraphs(test);
+console.log(constructJsonData(test['url1']));
