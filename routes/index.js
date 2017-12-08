ea1deaabcae735db1c3dba96964bb3d259cd4bbe
Nathan Casals <nathan.casals@smartprospective.com>
2017-12-08 07:42:54 +0100

    Added graph display with d3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require('path');
 const debug = require('debug')('MemeMQL:express');
 const data = require('../data/d3_test_data');
-
+const {constructJsonData} = require('../lib/graph.js');
+const test = {
+    "url1": [["s1", "p1", "o1"], ["s2", "p2", "s1"], ["s1", "p3", "s2"],["s1", "p4", "o1"]],
+    "url2": [["s3", "p3", "o2"], ["s2", "p2", "o1"], ["s4", "p3", "o2"]]
+};
 /* GET home page. */
 router.get('/', function (req, res, next) {
     debug("dirname : " + __dirname);
@@ -13,6 +17,7 @@ router.get('/', function (req, res, next) {
 
 /* GET users listing. */
 router.get('/test', function (req, res, next) {
+    console.log(constructJsonData(test['url1']));
     res.json(data);
 });
 
